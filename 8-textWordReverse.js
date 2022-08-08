7ca5eb7be8b1e471dab673c94d123c6128c68a91
TFMahabub <rjmahabub543@gmail.com>
2022-08-09 00:02:26 +0600

    new file add

---
+++ 8-textWordReverse.js
@@ -0,0 +1,32 @@
+
+function reverseIndex(text){
+    textReverse = "";
+    for(let i = text.length-1; i >= 0; i--){
+        elements = text[i];
+        textReverse = textReverse + elements;
+    }
+    return textReverse;
+}
+
+
+
+let sentense = "I am a good boy";
+reversText = reverseIndex(sentense);
+console.log(reversText);
+
+
+
+function wordReverse(text){
+    let getWord = text.split(" ");
+    let pushWord = [];
+    for(let i = getWord.length-1; i >= 0; i--){
+        elements = getWord[i];
+        pushWord.push(elements);
+    }
+    wordJoin = pushWord.join(" ");
+    return wordJoin;
+}
+
+let sentense = "I am a good boy";
+result = wordReverse(sentense);
+console.log(result);
---
+++ 9-Fibonacci.js
@@ -0,0 +1,39 @@
+
+let fibo = [1, 2];
+for(let i = 2; i<= 30; i++){
+    fibo[i] = fibo[i-1] + fibo[i-2];
+}
+console.log(fibo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
